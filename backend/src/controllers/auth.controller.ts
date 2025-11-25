0c45b9afd3ea92b3d52996ad89956e7e542d38dd
tomjid <tomjidhua04@gmail.com>
2025-11-25 15:36:48 +0600

    feat: Add initial auth controller with a register endpoint.

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+
+export const registerController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const body = req.body;
+  }
+);
